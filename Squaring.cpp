3a2805e4a2242e58b15c3254dec9d79e114e78ee
Vitalya <vitalyabes.vb@gmail.com>
2024-02-05 09:44:04 +0300

    Initial commit

---
+++ Squaring.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+template <class T>
+T squaring(T el) {
+	return el * el;
+}
+
+template <>
+std::vector<int> squaring(std::vector<int> vec) {
+	for (int i = 0; i < vec.size(); i++)
+		vec[i] = vec[i] * vec[i];
+	return vec;
+	
+}
+
+int main()
+{
+	int elem = 7;
+	std::vector<int> vec = { 1, 3, 9, -3, 12 };
+	std::cout << squaring(elem) << "\n";
+	vec = squaring(vec);
+	for (int i = 0; i < vec.size(); i++)
+		std::cout << vec[i] << ' ';
+	
+}
